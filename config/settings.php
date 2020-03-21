77484a94fc8aa9729bfadb681e435fcf6a9bd66b
faramarz mhrnami <mfaramarz1991@gmail.com>
2020-03-22 03:43:24 +0430

    Update settings.php

--- config/settings.php
+++ config/settings.php
@@ -5,7 +5,7 @@ error_reporting(0);
 ini_set('display_errors', '0');
 
 // Timezone
-date_default_timezone_set('Europe/Berlin');
+date_default_timezone_set('Asia/Tehran');
 
 // Settings
 $settings = [];
